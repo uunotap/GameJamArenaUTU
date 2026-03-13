c3a2a2b0193c9c20d771d21b309dca90ad5b3d47
Uuno Tapper <47410836+uunotap@users.noreply.github.com>
2026-03-13 21:02:23 +0200

    not working dash truely

--- entities/player/character_body_3d.gd
+++ entities/player/character_body_3d.gd
@@ -4,7 +4,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export var enemy: Node3D
-
 @export var target: Node3D
 
 
@@ -27,23 +26,34 @@ func _physics_process(delta: float) -> void:
 			target=enemy
 		else:
 			target=null
-	
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		var dashspeed = 1
-		if Input.is_action_just_pressed("dash_left"):
-			dashspeed=10
-		if Input.is_action_just_pressed("dash_right"):
-			dashspeed=10
-		velocity.x = direction.x * (SPEED+dashspeed)
-		velocity.z = direction.z * (SPEED+dashspeed)
+
+		velocity.x = direction.x * (SPEED)
+		velocity.z = direction.z * (SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
+	var dash_dir:Vector3
+	if Input.is_action_pressed("dash_left"):
+		print("dash left")
+		dash_dir=(transform.basis * Vector3(-1, 0, -1)).normalized()
+		velocity.x = (dash_dir.x) * 50
+		velocity.z = (dash_dir.y) * 50
+		
+		
+	if Input.is_action_pressed("dash_right"):
+		print("dash right")
+		dash_dir=(transform.basis * Vector3(1, 0, -1)).normalized()
+		velocity.x = (dash_dir.x) * 50
+		velocity.z = (dash_dir.y) * 50
+		
+		
 	if target != null:
 		look_at(target.position)
 
